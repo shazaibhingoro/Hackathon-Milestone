4fac8575d5b9917779cc3749ba0d3995bcdc8d74
shazaibhingoro <shazaibhingoro290@gmail.com>
2024-12-13 09:47:58 +0500

    Add files via upload

---
+++ milestone1-2/script.js
@@ -0,0 +1,10 @@
+var toogleButton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+toogleButton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
+    }
+    else {
+        skills.style.display = 'none';
+    }
+});
